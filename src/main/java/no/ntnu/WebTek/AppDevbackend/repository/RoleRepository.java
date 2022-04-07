4a4307e106416cba33ad7e24bd9bc95e2e0a9c18
Jones <jonesma@ntnu.no>
2022-04-07 23:29:59 +0200

    Revert "Added classes in model and repository"
    This reverts commit 06ed8b1570d036c8eeef52e4bc009d073f046673.

--- src/main/java/no/ntnu/WebTek/AppDevbackend/repository/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package no.ntnu.WebTek.AppDevbackend.repository;
-
-import no.ntnu.WebTek.AppDevbackend.model.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-    Role findByName(String name);
-}
--- src/main/java/no/ntnu/WebTek/AppDevbackend/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package no.ntnu.WebTek.AppDevbackend.repository;
-
-import no.ntnu.WebTek.AppDevbackend.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUserName(String username);
-}
