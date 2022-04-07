9887f520aa5fd02dcf7bbd08a0851ffd4f1034e0
Jones <jonesma@ntnu.no>
2022-04-07 22:34:55 +0200

    initial commit

---
+++ src/test/java/no/ntnu/WebTek/AppDevbackend/ApplicationTests.java
@@ -0,0 +1,13 @@
+package no.ntnu.WebTek.AppDevbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
