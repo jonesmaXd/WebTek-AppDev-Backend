26514aa4fd4599c28101739413adbd333bcb1ee0
Eirik <eiriknd@stud.ntnu.no>
2022-04-08 02:48:40 +0200

    SecurityConfig in security package
    created security package with authentication, users with different levels of authorization and authorities.

--- src/main/java/no/ntnu/WebTek/AppDevbackend/Application.java
+++ src/main/java/no/ntnu/WebTek/AppDevbackend/Application.java
@@ -1,13 +1,45 @@
 package no.ntnu.WebTek.AppDevbackend;
 
+import no.ntnu.WebTek.AppDevbackend.model.Role;
+import no.ntnu.WebTek.AppDevbackend.model.User;
+import no.ntnu.WebTek.AppDevbackend.service.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	CommandLineRunner run(UserService userService) {
+		return args -> {
+			userService.saveRole(new Role(null,"ROLE_USER"));
+			userService.saveRole(new Role(null,"ROLE_MEMBER"));
+			userService.saveRole(new Role(null,"ROLE_ADMIN"));
+
+			userService.saveUser(new User(null, "Eirik Dahle", "eirikd", "1234", new ArrayList<>()));
+			userService.saveUser(new User(null, "Jones Maaroufi", "jonesma", "1234", new ArrayList<>()));
+			userService.saveUser(new User(null, "Ola Jens", "olaj", "1234", new ArrayList<>()));
+
+			userService.addRoleToUser("eirikd", "ROLE_ADMIN");
+			userService.addRoleToUser("jonesma", "ROLE_MEMBER");
+			userService.addRoleToUser("olaj", "ROLE_USER");
+
+		};
 	}
 
 }
--- src/main/java/no/ntnu/WebTek/AppDevbackend/service/UserServiceImplementation.java
+++ src/main/java/no/ntnu/WebTek/AppDevbackend/service/UserServiceImplementation.java
@@ -6,16 +6,38 @@ import no.ntnu.WebTek.AppDevbackend.model.Role;
 import no.ntnu.WebTek.AppDevbackend.model.User;
 import no.ntnu.WebTek.AppDevbackend.repository.RoleRepository;
 import no.ntnu.WebTek.AppDevbackend.repository.UserRepository;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Service @RequiredArgsConstructor @Transactional @Slf4j
-public class UserServiceImplementation implements UserService {
+public class UserServiceImplementation implements UserService, UserDetailsService {
 
     private final UserRepository userRepo;
     private final RoleRepository roleRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepo.findByUserName(username);
+        if (user == null){
+            log.error("USer not found in the database");
+            throw new UsernameNotFoundException("User not found in the database");
+        } else {
+            log.info("User not found in the database: {}", username);
+        }
+        Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        user.getRoles().forEach(role -> {
+            authorities.add(new SimpleGrantedAuthority(role.getName()));
+        });
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), authorities);
+    }
 
     @Override
     public User saveUser(User user) {
@@ -56,4 +78,6 @@ public class UserServiceImplementation implements UserService {
         log.info("Getting all users {}");
         return userRepo.findAll();
     }
+
+
 }
